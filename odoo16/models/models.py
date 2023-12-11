36bcb1884141f574ff125cf44957c114d0fee2f1
maazds <maazaslam628@gmail.com>
2023-12-11 16:42:53 +0500

    archive status

--- odoo16/models/models.py
+++ odoo16/models/models.py
@@ -6,6 +6,8 @@ class odoo16(models.Model):
     _name = 'odoo.16'
     _description = 'odoo16 odoo16'
 
-    name = fields.Char()
-    value = fields.Integer()
-    description = fields.Text()
+    name = fields.Char('Name')
+    age = fields.Integer('Age')
+    ref = fields.Char('Ref')
+    gender = fields.Selection([('0','Male'),('1','Female')],'Gender',required=True)
+    active = fields.Boolean('Active' , default=True)
