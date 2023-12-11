72168f20aff59998a804c5cc4188cd125f9e0d95
maazds <maazaslam628@gmail.com>
2023-12-11 11:28:37 +0500

    testing

--- odoo16/models/models.py
+++ odoo16/models/models.py
@@ -1,18 +1,11 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
+class odoo16(models.Model):
+    _name = 'odoo16.odoo16'
+    _description = 'odoo16 odoo16'
 
-# class odoo16(models.Model):
-#     _name = 'odoo16.odoo16'
-#     _description = 'odoo16.odoo16'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char()
+    value = fields.Integer()
+    description = fields.Text()
